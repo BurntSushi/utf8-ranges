c8be68013603174e850f36277f177eb25d6602e4
Andrew Gallant <jamslam@gmail.com>
2022-04-04 14:55:53 -0400

    style: replace 'unsafe' with a safe API
    This was just code that was older than 'from_ref', which was stabilized
    in Rust 1.28.

--- src/lib.rs
+++ src/lib.rs
@@ -152,7 +152,7 @@ impl Utf8Sequence {
     pub fn as_slice(&self) -> &[Utf8Range] {
         use self::Utf8Sequence::*;
         match *self {
-            One(ref r) => unsafe { slice::from_raw_parts(r, 1) },
+            One(ref r) => slice::from_ref(r),
             Two(ref r) => &r[..],
             Three(ref r) => &r[..],
             Four(ref r) => &r[..],
